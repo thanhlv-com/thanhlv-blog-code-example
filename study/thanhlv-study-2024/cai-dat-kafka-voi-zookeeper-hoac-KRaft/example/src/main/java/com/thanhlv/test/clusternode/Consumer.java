822d3268808485bca4fba2812b6f22c679ddf301
Your Name <you@example.com>
2024-06-14 00:27:21 +0700

    a

---
+++ study/thanhlv-study-2024/cai-dat-kafka-voi-zookeeper-hoac-KRaft/example/src/main/java/com/thanhlv/test/clusternode/Consumer.java
@@ -0,0 +1,49 @@
+package com.thanhlv.test.clusternode;
+
+import lombok.var;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Consumer {
+    public static void main(String[] args) {
+        newThreadConsumer(createPropertiesByGroupId("1"),"app-1");
+        while (true) {
+
+        }
+    }
+
+    public static void newThreadConsumer(Properties props,String name) {
+        new Thread(() -> {
+            // create consumer
+            KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+
+            consumer.subscribe(Arrays.asList("my-topic-2"));
+            // poll for new data
+            while (true) {
+                ConsumerRecords<String, String> records =
+                        consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println("name: " + name + " , Group_id: " + props.get(ConsumerConfig.GROUP_ID_CONFIG) + " , Key: " + record.key() + ", Value: " + record.value() + ", Partition: " + record.partition() + ", Offset:" + record.offset());
+                }
+            }
+        }).start();
+    }
+
+    public static Properties createPropertiesByGroupId(String groupId) {
+        final var props = new Properties();
+        props.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, "java-Consumer");
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092,localhost:29093");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        return props;
+    }
+}
---
+++ study/thanhlv-study-2024/cai-dat-kafka-voi-zookeeper-hoac-KRaft/example/src/main/java/com/thanhlv/test/clusternode/Producer.java
@@ -0,0 +1,45 @@
+package com.thanhlv.test.clusternode;
+
+import lombok.var;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+public class Producer {
+
+    public static void main(String[] args) throws IOException {
+
+        final var props = new Properties();
+        props.setProperty(ProducerConfig.CLIENT_ID_CONFIG, "java-producer");
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092,localhost:29093");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        try (var producer = new KafkaProducer<String, String>(props)) {
+
+            System.out.println("Start sending messages ... ");
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));) {
+                String message;
+                do {
+                    System.out.print("Enter message: ");
+                    message = br.readLine().trim();
+                    final var messageProducerRecord = new ProducerRecord<>(
+                            "my-topic-2",     //topic name
+                            "key-" + message,            // key
+                            "message: " + message        // value
+                    );
+                    producer.send(messageProducerRecord);
+
+                } while (!message.equalsIgnoreCase("close"));
+            }
+        }
+
+    }
+
+}
