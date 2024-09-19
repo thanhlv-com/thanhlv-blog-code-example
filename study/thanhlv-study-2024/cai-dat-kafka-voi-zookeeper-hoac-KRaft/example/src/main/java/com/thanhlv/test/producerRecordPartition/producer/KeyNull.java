2dadaccb68883f6cdcf5137ae4ca4204ce49a3a6
Thanhlv <lethanh9398@gmail.com>
2024-09-19 16:54:13 +0700

    a

--- study/thanhlv-study-2024/cai-dat-kafka-voi-zookeeper-hoac-KRaft/example/src/main/java/com/thanhlv/test/producerRecordPartition/producer/KeyNull.java
+++ study/thanhlv-study-2024/cai-dat-kafka-voi-zookeeper-hoac-KRaft/example/src/main/java/com/thanhlv/test/producerRecordPartition/producer/KeyNull.java
@@ -1,34 +1,38 @@
 package com.thanhlv.test.producerRecordPartition.producer;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.Metric;
+import org.apache.kafka.common.MetricName;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
 
 @Slf4j
 public class KeyNull {
+    @SneakyThrows
     public static void main(String[] args) throws IOException {
-
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "ALL");
         final var props = new Properties();
         props.setProperty(ProducerConfig.CLIENT_ID_CONFIG, "java-producer-producerRecordPartition-KeyNotNull");
         props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29091,localhost:29092,localhost:29093,localhost:29094");
         props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        // Khi tong so luong data gui = BATCH_SIZE_CONFIG thi se tinh toan lai partition moi
         props.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, "20000");
+        props.setProperty(ProducerConfig.LINGER_MS_CONFIG, "0");
 
         try (var producer = new KafkaProducer<Object, String>(props)) {
             try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));) {
-
                 while (true) {
-                    log.info("Enter number random message: ");
+                    log.debug("Enter number random message: ");
                     String number = br.readLine().trim();
                     Long start = System.currentTimeMillis();
                     log.info("Start: {} ms",start);
@@ -38,13 +42,12 @@ public class KeyNull {
                     );
                     for (int i = 0; i < Integer.parseInt(number); i++) {
                         producer.send(messageProducerRecord);
+                        Thread.sleep(1);
                     }
                     Long end = System.currentTimeMillis();
                     log.info("END: {} ms and end - start = {}",end,end - start);
-
                 }
             }
         }
-
     }
 }
