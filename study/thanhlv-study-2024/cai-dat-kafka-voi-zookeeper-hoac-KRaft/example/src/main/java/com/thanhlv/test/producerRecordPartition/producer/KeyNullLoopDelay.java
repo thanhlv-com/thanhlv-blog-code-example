2915282bc33d8e30159f2ad8730128799a0d9d20
Thanhlv <lethanh9398@gmail.com>
2024-09-25 11:40:46 +0700

    a

---
+++ study/thanhlv-study-2024/cai-dat-kafka-voi-zookeeper-hoac-KRaft/example/src/main/java/com/thanhlv/test/producerRecordPartition/producer/KeyNullLoopDelay.java
@@ -0,0 +1,46 @@
+package com.thanhlv.test.producerRecordPartition.producer;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
+public class KeyNullLoopDelay {
+    @SneakyThrows
+    public static void main(String[] args) throws IOException {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "ALL");
+        final var props = new Properties();
+        props.setProperty(ProducerConfig.CLIENT_ID_CONFIG, "java-producer-producerRecordPartition-KeyNotNull");
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29091,localhost:29092,localhost:29093,localhost:29094");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, "5000");
+        // 5s
+        props.setProperty(ProducerConfig.LINGER_MS_CONFIG, "5000");
+
+        try (var producer = new KafkaProducer<Object, String>(props)) {
+            while (true) {
+                final var messageProducerRecord = new ProducerRecord<>(
+                        "topic-rep-1-partition-10",     //topic name
+                        // 36 byte
+                        UUID.randomUUID().toString()        // value
+                );
+                Integer numberSend = 114;
+                for (int i = 1; i <= numberSend; i++) {
+                    producer.send(messageProducerRecord);
+                }
+                // 10s
+                Thread.sleep(10000);
+            }
+        }
+    }
+}
